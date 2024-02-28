d4a0482243b84ed2d7855c61aa7873cf945fe92c
Giorgos Rodopoulos <up1083876@ac.upatras.gr>
2024-02-29 01:53:44 +0200

    Implemented get_web_server and get_cookies functions.

--- lab01.py
+++ lab01.py
@@ -28,12 +28,42 @@ def get_headers_from_url(url: str) -> dict:
         return None
 
 
+def get_web_server(url: str) -> str:
+    try:
+        response = requests.get(url)
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            return response.headers.get('Server', 'Unknown')
+        else:
+            print(f"Failed to retrieve web server. Status code: {response.status_code}")
+            return None
+    except requests.RequestException as e:
+        print(f"Error: {e}")
+        return None
+    
+
+def get_cookies(url: str) -> list:
+    try:
+        response = requests.get(url)
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            return response.cookies
+        else:
+            print(f"Failed to retrieve cookies. Status code: {response.status_code}")
+            return None
+    except requests.RequestException as e:
+        print(f"Error: {e}")
+        return None
+
 if __name__ == "__main__":
     url = input('Enter a URL: ')
-    headers = get_headers_from_url(url)
 
-    if headers:
-        for header, value in headers.items():
-            print(f"{header}: {value}")
-    else:
-        print("Failed to retrieve HTML headers from the specified URL.")
+    if web_server := get_web_server(url):
+        print(f'Web server: {web_server}')
+
+    if cookies := get_cookies_from_url(url):
+        print('Cookies:')
+        for cookie in cookies:
+            print(f'\tName: {cookie.name}')
+            print(f'\tExpires: {cookie.expires}\n')
+
